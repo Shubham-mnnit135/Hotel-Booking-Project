c6a01d27cba9b48123ea010bdbe0b8bccd5779cc
Shubham Kumar <shubham.mnnit135@gmail.com>
2023-04-20 14:39:12 +0530

    Navbar and Header

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../../compnents/header/Header"
+import Navbar from "../../compnents/navbar/Navbar"
+import "./home.css"
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/pages/list/List.jsx
@@ -0,0 +1,14 @@
+import Header from "../../compnents/header/Header"
+import Navbar from "../../compnents/navbar/Navbar"
+import "./list.css"
+
+const List = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header type="list"/>
+    </div>
+  )
+}
+
+export default List
