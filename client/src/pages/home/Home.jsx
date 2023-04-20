91635acc99a8782391323b2ad433651bb6ad1361
Shubham Kumar <shubham.mnnit135@gmail.com>
2023-04-20 22:45:04 +0530

    Home_Page_Completed

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,10 @@
+import Featured from "../../compnents/featured/Featured"
+import FeaturedProperty from "../../compnents/featuredProperty/FeaturedProperty"
+import Footer from "../../compnents/footer/Footer"
 import Header from "../../compnents/header/Header"
+import MailList from "../../compnents/mailList/MailList"
 import Navbar from "../../compnents/navbar/Navbar"
+import PropertyList from "../../compnents/propertyList/PropertyList"
 import "./home.css"
 
 const Home = () => {
@@ -7,8 +12,19 @@ const Home = () => {
     <div>
       <Navbar/>
       <Header/>
+      <div className="homeContainer">
+         <Featured/>
+         <h1 className="homeTitle">Browe by property type</h1>
+         <PropertyList/>
+         <h1 className="homeTitle">Home guests love</h1>
+         <FeaturedProperty/>
+         <MailList/>
+         <Footer/> 
+      </div>
+      {/* <MailList/>
+      <Footer/>  */}
     </div>
   )
 }
 
-export default Home
+export default Home;
